ffd89c986fe1ace7d6eaca3991303e2a28bd7918
vectariano <victorevstigneev36@gmail.com>
2025-10-29 18:53:34 +0300

    feat: доработал дизайн карточек отелей

--- wego/src/components/ListOfHotels.jsx
+++ wego/src/components/ListOfHotels.jsx
@@ -25,11 +25,32 @@ function ListOfHotels() {
                     <img src={hotel.images?.[0]?.thumbnail} className="hotel-image" alt={hotel.name}/>
                     <div className="hotel-info-flex">
                         <div className="card-head">
-                            <h1 className="hotel-name">{hotel.name}</h1>
-                            <h1 className="hotel-price">{hotel.total_rate.lowest}</h1>
+                            <div className="name-description">
+                                <h1 className="hotel-name">{hotel.name}</h1>
+                                <div className="card-mid">
+                                    <h2 className="hotel-description">{hotel.description}</h2>
+                                </div>
+                            </div>
+                            <div className="hotel-rating">
+                                <h2 className="rating">{hotel.overall_rating}</h2>
+                                <h2 className="reviews">{hotel.reviews} reviews </h2>
+                            </div>
                         </div>
-                        <h2>Rating: {hotel.overall_rating}</h2>
-                        <h2>Reviews ({hotel.reviews})</h2>
+                        <div className="card-bottom">
+                            <div className="amenities-grid">
+                                <ul className="amenities-list">
+                                    {hotel.amenities.map((amenity, i) => (
+                                        <li key={i} className="hotel-amenities">{amenity}</li>
+                                    ))}
+                                </ul>
+                                
+                            </div>
+                            <div className="book-item">
+                                <h1 className="hotel-price">{hotel.total_rate.lowest}</h1>
+                                <button className="booking-button">Book now</button>
+                            </div>
+                        </div>
+                        
                     </div>
                 </div>
             ))}
